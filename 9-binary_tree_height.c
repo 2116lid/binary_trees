3923a10260d8a7ff3173a2fdf0094f8a66c37c57
Noah Alemayehu <noahlem21@gmail.com>
2024-01-04 07:19:39 -0800

    fix for 6and9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,12 +3,15 @@
 /**
  * binary_tree_height - a function that measures the height of a binary tree.
  * @tree: a pointer to the root node of the tree to measure the height.
+ *
+ * Return: the height on succssess, 0 if NULL(tree).
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
 		size_t i = 0, j = 0;
+
 		i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
